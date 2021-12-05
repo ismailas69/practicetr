e7936726700f422e5ec8f572d86cd5de8bd627e3
ismailas69 <ismailasan@gmail.com>
2021-12-05 07:56:54 -0500

    "first commit"

---
+++ eclipse-workspace/practicetr/src/Q04.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Q04 {
+	// verilen bir int Arrayde toplamlari istenen degere esit olan sayi ciftlerini yazdiran bir method yaziniz
+	// int[]arr = {5,7,-6,4,2,15,3,8,1};
+	// int istenentoplam=9;
+	
+	public static void main(String[] args) {
+		
+		int[]arr = {5,7,-6,4,2,15,3,8,1};
+		
+		System.out.println(Arrays.toString(arr));
+		int istenenToplam=9;
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+			
+				if (arr[i]+arr[j]==istenenToplam) {
+				System.out.println(arr[i]+" ile "+arr[j]+ " toplami " + istenenToplam);
+			}
+				
+			}
+			
+		}
+		
+
+	}
+
+}
